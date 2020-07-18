b114e0b13f4929c7cf4855a918ea6e4306194fe5
Maksym Bolshak <maksym.bolshak@mckesson.com>
2020-07-18 15:54:55 +0300

    Add controller

---
+++ src/main/java/com/bolshak/vendingmashine/VendingMashineApplication.java
@@ -0,0 +1,13 @@
+package com.bolshak.vendingmashine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMashineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMashineApplication.class, args);
+	}
+
+}
