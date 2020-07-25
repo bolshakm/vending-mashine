47bfaa5b74b601a19b27a0d635e42025e4a97ee7
Maksym Bolshak <maksym.bolshak@mckesson.com>
2020-07-25 19:47:25 +0300

    refactoring

--- src/main/java/com/bolshak/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/com/bolshak/vendingmachine/controller/VendingMachineController.java
@@ -10,6 +10,7 @@ import com.bolshak.vendingmachine.service.UserService;
 import com.bolshak.vendingmachine.service.VendingMachineHasProductService;
 import com.bolshak.vendingmachine.service.VendingMachineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +35,7 @@ public class VendingMachineController {
 	@Autowired
 	private VendingMachineHasProductService vendingMachineHasProductService;
 
+	@PreAuthorize("hasAnyAuthority('ADMIN')")
 	@GetMapping("/vending-machine")
 	public String get(Model model) {
 		List<VendingMachine> vendingMachines = vendingMachineServiceImpl.findAll();
